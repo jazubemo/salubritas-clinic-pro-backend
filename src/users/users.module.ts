faa2cb254dcc2627e5eebcd5c6769510e5c903e1
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-05-25 10:45:37 -0600

    feat: finalize set up to use @current-user decorator

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -9,5 +9,6 @@ import { UserService } from './user.service';
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   providers: [UsersResolver, UserService],
+  exports: [UserService],
 })
 export class UsersModule {}
