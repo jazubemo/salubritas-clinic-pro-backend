eceefbfb65fe462e12fb982404e448e9cbeeee77
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-06-03 11:15:37 -0600

    fix: clinicMemberships and ClinicMembership schemas to be an array

--- src/users/schemas/clinic-membership.schema.ts
+++ src/users/schemas/clinic-membership.schema.ts
@@ -2,17 +2,20 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
 import { Status } from './../enums/status.enum';
 import { Role } from './../enums/role.enum';
-import { Field } from '@nestjs/graphql';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 
+@ObjectType()
 @Schema({ _id: true, timestamps: true })
 export class ClinicMembership {
+  @Field(() => ID)
   @Prop({ type: Types.ObjectId, ref: 'Clinic', required: true })
   clinicId!: Types.ObjectId;
 
+  @Field(() => String)
   @Prop({ required: true })
   name!: string;
 
-  @Field({
+  @Field(() => Status, {
     description:
       'The current operational status of the user account. Allowed values: ACTIVE, ARCHIVED.',
   })
@@ -23,6 +26,7 @@ export class ClinicMembership {
   })
   status!: Status;
 
+  @Field(() => [Role])
   @Prop({ type: [String], required: true, enum: Role })
   roles!: Role[];
 }
