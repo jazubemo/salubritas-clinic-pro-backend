02f5a16e991852d1cba2cafd3635326d5c2f4417
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-06-16 10:54:12 -0600

    fix: set a clear name for the minimal and maximum durations of the clinic appointments

--- src/appointments/dto/create-appointment.input.ts
+++ src/appointments/dto/create-appointment.input.ts
@@ -5,6 +5,10 @@ import { IsDate, IsIn, IsNotEmpty, MinDate } from 'class-validator';
 import { AppointmentStatus } from '../enums/appointment-status.enum';
 import { IsAfter } from '../decorators/is-after.decorator';
 import { IsBetween } from '../decorators/is-between.decorator';
+import {
+  MAX_APPOINTMENT_DURATION_MINUTES,
+  MIN_APPOINTMENT_DURATION_MINUTES,
+} from 'src/common/constants/app.constants';
 
 @InputType()
 export class CreateAppointmentInput extends OmitType(
@@ -23,9 +27,14 @@ export class CreateAppointmentInput extends OmitType(
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
-  @IsBetween('startTime', 15, 60, {
-    message: `The appointment should last between 15 and 60 minutes`,
-  })
+  @IsBetween(
+    'startTime',
+    MIN_APPOINTMENT_DURATION_MINUTES,
+    MAX_APPOINTMENT_DURATION_MINUTES,
+    {
+      message: `The appointment should last between ${MIN_APPOINTMENT_DURATION_MINUTES} and ${MAX_APPOINTMENT_DURATION_MINUTES} minutes`,
+    },
+  )
   @IsAfter('startTime', {
     message: `The appointment's end date must be later than start date.`,
   })
--- src/common/constants/app.constants.ts
+++ src/common/constants/app.constants.ts
@@ -2,3 +2,5 @@ export const APP_TIMEZONE = 'America/Tegucigalpa';
 
 //time-related
 export const ONE_MINUTE_IN_MILLISECONDS = 60 * 1000;
+export const MIN_APPOINTMENT_DURATION_MINUTES = 15;
+export const MAX_APPOINTMENT_DURATION_MINUTES = 60;
