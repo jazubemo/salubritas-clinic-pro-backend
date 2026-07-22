7efe7d80fb6898d33b88e1f73fcfe60a82c1296f
Jasmin Benitez <jasminazucenabenitez@gmail.com>
2026-07-22 14:08:25 -0600

    feat: initial prisma setup

---
+++ prisma/migrations/20260722200554_optimize_user_fields/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" UUID NOT NULL,
+    "dni" VARCHAR(13) NOT NULL,
+    "first_name" VARCHAR(150) NOT NULL,
+    "last_name" VARCHAR(150) NOT NULL,
+    "auth_id" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_dni_key" ON "users"("dni");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_auth_id_key" ON "users"("auth_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
